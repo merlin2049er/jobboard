d27629ef23c740ef1bd6753a0b77129088aca4e0
Joe Guerra <guerra_joe@hotmail.com>
2018-11-21 12:24:58 -0800

    ..

--- Gemfile
+++ Gemfile
@@ -42,8 +42,6 @@ gem 'haml', '~> 5.0', '>= 5.0.4'
 gem 'jquery-rails'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 
-gem 'ransack', '~> 2.1'
-
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_21_193913) do
+ActiveRecord::Schema.define(version: 2018_11_21_194055) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_11_21_193913) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
   end
 
 end
